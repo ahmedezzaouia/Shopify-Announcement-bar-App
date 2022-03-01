00979695c07c0f0abe59cec4bbeaf3124ff9b49e
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2022-03-01 22:32:24 +0100

    fix font size

--- pages/index.js
+++ pages/index.js
@@ -13,20 +13,19 @@ const shopQuery = gql`
 export default function Index() {
   return (
     <Page>
-      <Heading>Announcemnet Bar</Heading>
+      <Heading>Announcement Bar</Heading>
       <div className="container">
         <h2 className="title">
           To enable this app in your theme just clik the button bellow{" "}
         </h2>
 
         <img
-          src="https://lh3.googleusercontent.com/hFZPWQqWRlydMlFb2PcM0R9T5EiYZiXeZ_Vw-yYoDZXZvZ2GFRFGLpHibn7_lEFq5LyTfpixoT9Q92JCIKa_pT0Gq_Mx2_KxVarXrJ_4oQ89xwXYa2X28BqDGCHS1RppbbVgmqHnsKzg45MPVpYn7zOgRhcmX3ZbNAvsIGM5h_0pRAHfUgBdMbY9bhQj-CTzms1PC2hSNIfprb7cCTkgFAFg7j9-nbGxzi9r4FMYBLcQImajMhLuFAxMl62xxDDLRm8qDHGrPLZLPv-hbNbgyOjMkIx7VHqxjiGZP-LioOjp--91xCDZGtqwiTtMCaAeJwXdHwVZEEOoIfz5DPVEV6tirRHXHcGNM0f5WCF3EgvyxtDYBERnC6_3TTRe8DqfaFLDdL0F5QL7uV5WQM5kcyeJm_KRUoBOAdIiFnGHg2sUGe_wOLqAgEiUw7iqNoZ4N0s1is9z8LIejbtWzb0o-BU-Yf6B8QhezqGlgdi_jkpGEgwhVEjggOaBbx1Iz_Deqpmfvg4_y3PdgysYCpPMlvyLXmGRXE8wDXQYf87HxySiARr-p_w3_bcwG7PAg4kkqNMqVXXYSLZI31q_o2VAdHLdLHIqA-AE3dmXYvYfthswVOyCSZelNiVSn0dj3y_vhpd8Pt_-oZ7jxpH1GnVV9fiiQ1njcOuHsaDWucrHV-RAdvB-FIk3kbwNq6llHp4lpboJtdqUHjqjXh_yd8iJ1zXv=w282-h200-no?authuser=0"
+          src="https://i.ibb.co/whZ1M7W/screenbud-1cc27f3e-84ab-4aab-9104-9a185389f8ac.png"
           alt="enable guide"
           width={500}
           height={300}
         />
       </div>
-
       <Query query={shopQuery}>
         {({ data, loading, error }) => {
           if (loading) return <div>Loading…</div>;
@@ -49,23 +48,33 @@ export default function Index() {
       </Query>
 
       <style jsx>{`
-
-      .container{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+        .container {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
         .title {
           padding: 50px;
           text-align: center;
           font-size: large;
           font-family: fangsong;
+          border-radius: 91px;
+          background: #0a8954;
+    border-radius: 91px;
+    color: white;
+    margin-bottom: 20px;
         }
         .btn {
-          padding: 10px 22px;
+          padding: 17px 62px;
           color: black;
           cursor: pointer;
+          border: none;
+          border-radius: 30px;
+          background: darkgreen;
+          color: white;
+          font-size: 19px;
+          font-family: math;
         }
         .btn_container {
           padding: 20px;
